d06fa80b71b4d85214c9efba19bdcde84d875b32
wysockilukas <lukasz.wysocki@outlook.com>
2018-11-28 23:38:00 +0100

    oracle udalo sie !!

---
+++ Oracle/002/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "gopkg.in/goracle.v2"
+)
+
+func main() {
+
+	//db, err := sql.Open("goracle", "scott/tiger@10.0.1.127:1521/orclpdb1")
+	db, err := sql.Open("goracle", "hr/oracle@localhost:1521/orcl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select sysdate from dual")
+	if err != nil {
+		fmt.Println("Error running query")
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	var thedate string
+	for rows.Next() {
+
+		rows.Scan(&thedate)
+	}
+	fmt.Printf("The date is: %s\n", thedate)
+}
